ebd3208006d58297c8ecf8330232d7744e73dba4
gustavorodrigues27 <goncalves.gustavo27@escola.pr.gov.br>
2024-09-05 10:52:59 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("///////////////////");
+    System.out.println("/ Banda: Slipknot /");
+    System.out.println("///////////////////");
+System.out.println("------------------------------------------------------------------------------------------------");
+
+    System.out.println("");
+
+    System.out.println("AlbSystemum : lowa ");
+      System.out.println("Ano de lançamento: 2000");
+
+  System.out.println(" White Pony é o terceiro álbum de estúdio da banda americana de metal alternativo Deftones, lançado em 20 de junho de 2000, pela Maverick Records. Foi produzido por Terry Date, que produziu os dois primeiros álbuns da banda, Adrenaline e Around the Fur.   ");
+System.out.println("------------------------------------------------------------------------------------------------");
+  System.out.println("");
+
+  System.out.println("///////////");
+  System.out.println("/ Musicas /");
+  System.out.println("///////////");
+  System.out.println("");   
+System.out.println("------------------------------------------------------------------------------------------------");
+
+   System.out.println(" 1  Back to school                      03:58."); 
+    System.out.println(" 2  Feiticeira                          03:10.");
+    System.out.println(" 3  Digital Bath                        04:16.");
+    System.out.println(" 4  Elite                               04:02.");
+    System.out.println(" 5  Rx Queen                            04:29.");
+    System.out.println(" 6  Steet Carp                          02:42.");
+    System.out.println(" 7  Teenager                            03:21.");
+    System.out.println(" 8  Knife Prty                          04:50.");
+    System.out.println(" 9  Korea                               03:24.");
+    System.out.println(" 10 Passenger                           06:09.");
+    System.out.println(" 11 Change(in the house of lies)        05:00.");
+    System.out.println(" 12 Pink Maggit                         07:34.");
+    System.out.println("------------------------------------------------------------------------------------------------");    
+  }
+
+
+
+
+}
